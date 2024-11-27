2340a2c37b4c678528300b5f44464e1a06d20197
aluno <aluno@192.168.0.94>
2024-11-27 13:48:30 -0300

    Meu primeiro Commit - Primeiro Programa em JAVA

---
+++ primeiroprograma/src/primeiroprograma/Executavel.java
@@ -0,0 +1,12 @@
+package primeiroprograma;
+
+public class Executavel {
+
+	public static void main(String[] args) {
+		System.out.println("Olá mundo!");
+		System.out.println("Meu nome é Felipe");
+		
+	}
+	
+
+}
