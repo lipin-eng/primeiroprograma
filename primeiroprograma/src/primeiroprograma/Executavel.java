8e48a52d85ef9943a74baba104bfe4709162c337
aluno <aluno@192.168.0.94>
2024-11-29 10:37:56 -0300

    Segundo commit

--- primeiroprograma/src/primeiroprograma/Executavel.java
+++ primeiroprograma/src/primeiroprograma/Executavel.java
@@ -9,12 +9,14 @@ int inteiro;
 double numeroDecimal;
 String texto;
 
-//Atribuição de valores 
-inteiro = 5
-numeroDecimal = 5,5;
-texto = "Felipe"
+//Atribuição de valores as variáveis.
+inteiro = 5;
+numeroDecimal = 5.65;
+texto = "Felipe";
 
-		
+System.out.println("Nome: " + texto);
+System.out.println("Numero inteiro: " + inteiro);
+System.out.println("Numero decimal: " + numeroDecimal);	
 	}
 	
 
